9abf2d6e9963397d763bea63e2441525bce14b3d
adrianoclr <126690470+adrianoclr@users.noreply.github.com>
2023-03-06 15:50:48 -0300

    Criacao da Classe Teste

---
+++ teste-local1/src/main/java/com/mycompany/teste/local1/Teste.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.teste.local1;
+
+public class Teste {
+     public static String texto;
+             
+    public Teste() {
+         texto = "Hello Terra";
+         System.out.println("v1 = " + texto);
+        
+      
+        
+    }
+}
