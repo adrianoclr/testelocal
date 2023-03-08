d4445c41021d4822068a95f6c0c47854dde31bbf
adrianoclr <adriano.m.souza@accenture.com>
2023-03-07 22:27:32 -0300

    alterado para Marte

--- teste-local1/src/main/java/com/mycompany/teste/local1/TesteRemoto.java
+++ teste-local1/src/main/java/com/mycompany/teste/local1/TesteRemoto.java
@@ -3,6 +3,6 @@ package com.mycompany.teste.local1;
 public class TesteRemoto {
 
     public static void main(String[] args) {
-        System.out.println("Terra");
+        System.out.println("Marte");
     }
 }
