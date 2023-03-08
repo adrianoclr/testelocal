4f4e97462c5510364347f0025ae74f622acabf8a
adrianoclr <adriano.m.souza@accenture.com>
2023-03-07 21:51:19 -0300

    atualizando Testeremoto

--- teste-local1/src/main/java/com/mycompany/teste/local1/TesteLocal1.java
+++ teste-local1/src/main/java/com/mycompany/teste/local1/TesteLocal1.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.teste.local1;
 
-/**
- *
- * @author adriano.m.souza
- */
 public class TesteLocal1 {
 
     public static void main(String[] args) {
