cdebce2adcef2f38c6fd494603dd6c4a7d57e1a4
adrianoclr <adriano.m.souza@accenture.com>
2023-03-07 22:11:40 -0300

    alterado para Terra

--- teste-local1/src/main/java/com/mycompany/teste/local1/TesteRemoto.java
+++ teste-local1/src/main/java/com/mycompany/teste/local1/TesteRemoto.java
@@ -3,6 +3,6 @@ package com.mycompany.teste.local1;
 public class TesteRemoto {
 
     public static void main(String[] args) {
-        System.out.println("Teste Remoto");
+        System.out.println("Terra");
     }
 }
